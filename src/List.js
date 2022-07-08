f8b65c8866f79bd876b8ca527ae482a3fd571f88
Turgut Yılmaz <songulturgut0641@gmail.com>
2022-07-08 16:09:26 +0300

    birthday

---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+      {people.map((person) =>{
+        const { id, name, age, image} = person;
+        return(
+          <div key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h3>{name}</h3>
+              <p>{age} years</p>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  );
+};
+
+export default List;
